4764d0f01489d05d3a785e298fca09d6ca203114
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 16:23:27 +0000

    stuff2

--- db/migrate/20170614163830_create_portfolios.rb
+++ db/migrate/20170614163830_create_portfolios.rb
@@ -2,11 +2,11 @@ class CreatePortfolios < ActiveRecord::Migration[5.0]
   def change
     create_table :portfolios do |t|
       t.string :title
-      t.string :string
+      t.text:thumb_image
       t.string :subtitle
       t.text :body
       t.text :main_image
-
+      
       t.timestamps
     end
   end

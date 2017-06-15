97c94f5eac8b766e33a5e99a1053f055b276ca27
Jordan Hudgens <jordan@devcamp.com>
2017-06-15 18:02:06 +0000

    added portfolio items via resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614163830) do
+ActiveRecord::Schema.define(version: 20170615174936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,19 +25,19 @@ ActiveRecord::Schema.define(version: 20170614163830) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
-    t.string   "thumb_image"
     t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "thumb_image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
-
+  
   create_table "skills", force: :cascade do |t|
     t.string   "title"
-    t.integer  "percent_utilized"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.integer   "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

d0ce9b7bddc6ffb956bc3ac8dde46d10ace12916
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 14:42:19 +0000

    new skills and schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20170615174936) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-  
+
   create_table "skills", force: :cascade do |t|
     t.string   "title"
-    t.integer   "percent_utilized"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end

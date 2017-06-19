07650732a965c06bb6a2469f7f2e6e8546f168e7
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 15:37:25 +0000

    add skill and portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614163830) do
+ActiveRecord::Schema.define(version: 20170615174936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170614163830) do
     t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"
+    t.text     "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

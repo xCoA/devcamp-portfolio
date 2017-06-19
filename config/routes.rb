a5fcd8f735f2248a2c2c1b2eac0e57abed640a48
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 17:54:39 +0000

    Integrate new and create actions for portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :portfolios
+  
   get 'pages/about'
+  
   get 'pages/contact'
+
   get 'pages/home'
+  
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

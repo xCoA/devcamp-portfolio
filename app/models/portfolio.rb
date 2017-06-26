fc309722dece5b2ea8fc5b149b7b079abc6318b1
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 17:54:34 +0000

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,12 @@ validates_presence_of :title, :body, :main_image, :thumb_image
   end 
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on rails') }
+
+  after_initialize :set_defaults
+  
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+  end
 end
+

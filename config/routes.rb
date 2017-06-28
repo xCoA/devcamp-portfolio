7a6d507de018ac25c97a77f6186f78ff6f4e1b22
Jordan Hudgens <jordan@devcamp.com>
2017-06-28 17:09:20 +0000

    Implemented basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  
+  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   

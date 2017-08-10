6f5ce4407a679261410265a8ab68db9f2a8fb1c7
Jordan Hudgens <jordan@devcamp.com>
2017-08-10 17:11:16 +0000

    Added validation for topics in blogs and added a topic form element

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,27 +1,29 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all 
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
   def index
     if logged_in?(:site_admin)
-    @blogs = Blog.recent.all
-  else
-    @blogs = Blog.published.all
-    @page_title = "My Portfolio Blog"
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
+    @page_title = "My Portfolio Blog"
   end
+
   # GET /blogs/1
   # GET /blogs/1.json
   def show
     if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
-      
+
       @page_title = @blog.title
       @seo_keywords = @blog.body
-    else 
+    else
       redirect_to blogs_path, notice: "You are not authorized to access this page"
     end
   end
@@ -42,11 +44,9 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+        format.html { redirect_to @blog, notice: 'Your post is now live.' }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,10 +57,8 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -70,21 +68,21 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
       format.json { head :no_content }
     end
   end
-  
+
   def toggle_status
     if @blog.draft?
       @blog.published!
     elsif @blog.published?
       @blog.draft!
     end
-      
-    redirect_to blogs_url, notice: 'Post status has been updated'  
+        
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
-  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
@@ -93,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body, :text)
+      params.require(:blog).permit(:title, :body, :topic_id,)
     end
 end
